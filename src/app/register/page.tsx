0dfa1be6cefd1d2263a004c81725c4016c8aab9f
Matheus Tavares <matheustavares1a@gmail.com>
2025-05-13 20:49:31 -0300

    bugfix/auth

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -57,7 +57,7 @@ export default function RegisterScreen() {
   return (
     <form
       onSubmit={handleSubmit(onSubscribe)}
-      className="flex w-full mx-auto items-center justify-center min-h-dvh md:gap-16 bg-blue"
+      className="flex w-full mx-auto items-center justify-center min-h-dvh md:gap-16 bg-primary md:bg-blue"
     >
       <Toast />
       <div className="flex flex-col w-full md:w-2/4 p-8 gap-4 md:my-3 bg-primary md:rounded-2xl items-center justify-center">
